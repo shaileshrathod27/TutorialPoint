e0c8019992217be7bc4974f1d4d522f0f645214c
shaileshrathod27 <saileshrathod27@gmail.com>
2017-06-08 09:03:55 +0530

    Initial Commit

---
+++ shailesh.basic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  shailesh.basic
+//
+//  Created by Administrator on 08/06/17.
+//  Copyright © 2017 Administrator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
